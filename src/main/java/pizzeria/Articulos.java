aa02488d3f724ce8d7ca7fe1fde5f4a73f9e8e8e
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-16 12:43:03 +0200

    Implementa los artículos disponibles

--- src/main/java/pizzeria/Articulos.java
+++ src/main/java/pizzeria/Articulos.java
@@ -11,7 +11,18 @@ package pizzeria;
  */
 public enum Articulos {
     
-    PIZZA_BASE(1, "Pizza base", 5);
+    PIZZA_BASE(1, "Pizza base", 5),
+    PIZZA_LOBILLA(2, "Pizza lobilla",7),
+    PIZZA_PERICOLOSA(3, "Pizza pericolosa", 7.5),
+    BARBAKO(4,"Barbako",0.5),
+    VERDURA(5, "Verduritas", 0.5),
+    QUESO(6, "Queso", 0.5),
+    PEPERONI(7, "Peperoni", 0.5),
+    GAMBAS(8, "Gambas", 0.5),
+    ATUN(9,"Atún", 0.5),
+    PINNA(10, "Piña", 0.5),
+    BACON(11, "Bacon", 0.5),
+    JAMON(12, "Jamón", 0.5);
     
     private int referenciaArticulo;
     private String nombreArticulo;
@@ -33,6 +44,11 @@ public enum Articulos {
 
     public double getPrecioArticulo() {
         return precioArticulo;
+    }
+
+    @Override
+    public String toString() {
+        return this.getNombreArticulo() + ", que cuesta " + this.getPrecioArticulo() + "€";
     }
     
     
