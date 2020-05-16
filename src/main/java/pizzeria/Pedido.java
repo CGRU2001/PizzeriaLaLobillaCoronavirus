96fd6e4610c91dc78ff5d1ab2e906bd1e7b8dbdd
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-16 12:35:30 +0200

    Implementa el método nuevoArticulo

--- src/main/java/pizzeria/Pedido.java
+++ src/main/java/pizzeria/Pedido.java
@@ -21,9 +21,12 @@ public class Pedido {
         Random r = new Random();
         this.referencia = Integer.toString(r.nextInt(10000000));
         this.articulosComanda = new ArrayList<>();
-        this.subtotal = 0;
+        this.subtotal = 0.5; //Se añaden 50 céntimos de gastos de envío
     }
     
+    public void nuevoArticulo(Articulos a){
+        this.articulosComanda.add(a);
+    }
 
     
     
